3b026183e2bb8a6df7e64c3791d28771ac2d37ec
sarthak1991 <sarthakbatra@rocketmail.com>
2024-10-25 12:42:08 +0530

    adding constants file example

---
+++ helpers/CONSTS copy.js
@@ -0,0 +1,18 @@
+// IMPORTANT: Rename this file as CONSTS.js .. The project expects this file. 
+
+const CONSTANTS = {
+  APPID: "YOUR APP ID",
+  CHANNEL: "YOUR CHANNEL NAME",
+  METHOD: "POST",
+  customerID: "YOUR CUSTOMER ID (You'll find it on Agora Console)",
+  customerSecret: "YOUR CUSTOMER SECRET (You'll find it on Agora Console)",
+  rtcToken: "TEMP TOKEN FOR TESTING",
+  rtmpRegion: "YOUR REGION",
+  RTMPUrl: "YOUR LIVE PUSH REGION",
+  InjectUrl:"YOUR LIVE PULL URL",
+  expiry:300,
+  accessKey: "YOUR AWS ACCESS KEY",
+  secretKey: "YOUR AWS SECRET KEY"
+};
+
+export default CONSTANTS;
